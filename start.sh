401f0351e12aabb0f472ecfe4501262edcf25d81
Shweta Grover <shweta.grover@nagarro.com>
2019-11-25 13:34:04 +0530

    sh file permission

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ docker-compose down -v && docker-compose build && docker-compose up -d
 echo "-----Composer install----------"
 docker exec order_php composer install --prefer-dist
 
-docker exec order_php bash -c 'chmod 777 -R /var/www/'
+docker exec order_php bash -c 'chmod 777 -R /var/www/html'
 
 docker exec order_php php artisan config:cache
 
